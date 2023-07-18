79080ae87a7a6f003baf6e060a9d91004d208dc4
MarS-37 <master_sbo@mail.ru>
2023-07-18 20:36:03 +0300

    signup

--- chat_module_1/chat.h
+++ chat_module_1/chat.h
@@ -33,7 +33,7 @@ public:
 					displayHelp();
 				} 
 				else if (command == "/signup") {
-					
+					signUp();
 				}
 				else {
 					std::cout << "Неизвестная команда. Используйте /help для помощи." << std::endl;
@@ -108,11 +108,62 @@ private:
 	void displayHelp() {
 		std::cout << "Список команд:" << std::endl;
 		std::cout << "/help - список команд." << std::endl;
-		std::cout << " " << std::endl;
+		std::cout << "/signup - регистрация пользователя." << std::endl;
 		std::cout << " " << std::endl;
 		std::cout << " " << std::endl;
 		std::cout << " " << std::endl;
 		std::cout << " " << std::endl;
 	}
+
+	void signUp() {
+		if (loggedInUser) {
+			throw std::runtime_error("Вы уже вошли в систему. Используйте /logout.");
+		}
+
+		std::string login, name, password;
+		std::cout << "Придумайте уникальный логин: ";
+		std::cin >> login;
+
+		// Проверка логина на повтор
+		auto it = std::find_if(users.begin(), users.end(), [&](const user& user) {
+			return user.getLogin() == login;
+			});
+
+		if (it != users.end()) {
+			throw std::runtime_error("Логин уже существует. Пожалуйста, введите другой логин.");
+		}
+
+		std::cout << "Введите свое имя: ";
+
+		// Игнорировать символ новой строки, оставшийся в буфере ввода
+		std::cin.ignore();
+		std::getline(std::cin, name);
+
+		std::cout << "Введите пароль: ";
+		std::cin >> password;
+
+		// создаем объект для записи учетки
+		user newUser(login, name, password);
+
+		// в контейнер
+		users.push_back(newUser);
+
+		// пишем в файл
+		saveUsers();
+
+		// создаем файл server@username.json
+		std::ofstream userFile("server@" + login + ".json");
+		if (userFile) { // файл создан
+
+			// Инициализация пустого 
+			// JSON-объекта для пользователя
+			userFile << "{}";
+			userFile.close();
+		}
+
+		std::cout << "Новая учетная запись создана" << std::endl;
+	}
+
+
 };
 
